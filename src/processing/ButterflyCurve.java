9e9632ac0448ac5644107951793335325d6662cf
Suicolen <ristovaher1213@gmail.com>
2020-07-10 18:31:27 +0300

    Made butterfly curve more fun
    :)

--- src/processing/ButterflyCurve.java
+++ src/processing/ButterflyCurve.java
@@ -25,15 +25,6 @@ public class ButterflyCurve extends ShaderSketch {
         colorMode(HSB, 360, 100, 100);
 
         points = new ArrayList<>();
-        for(float theta = 0; theta < MAX_THETA; theta += 0.001) {
-            float r = exp(cos(theta)) - 2 * cos(4 * theta) + pow(sin(theta / 24f), 5);
-            float x = r * cos(theta);
-            float y = r * sin(theta);
-            float xx = (x * 60);
-            float yy = (y * 60);
-            float col = map(theta, 0, MAX_THETA, 0, 360);
-            points.add(new Point(xx, yy, col));
-        }
     }
 
     private float theta = 0f;
@@ -44,6 +35,17 @@ public class ButterflyCurve extends ShaderSketch {
     @Override
     public void draw() {
         background(0);
+        points.clear();
+        float increment = map(mouseX, 0, width, 0.001f, 0.05f);
+        for(float theta = 0; theta < MAX_THETA; theta += increment) {
+            float r = exp(cos(theta)) - 2 * cos(4 * theta) + pow(sin(theta / 24f), 5);
+            float x = r * cos(theta);
+            float y = r * sin(theta);
+            float xx = (x * 60);
+            float yy = (y * 60);
+            float col = map(theta, 0, MAX_THETA, 0, 360);
+            points.add(new Point(xx, yy, col));
+        }
         //stroke(255);
         strokeWeight(2);
         translate(width / 2f, height / 2f);
@@ -54,8 +56,7 @@ public class ButterflyCurve extends ShaderSketch {
         for (int i = 0; i < points.size(); i++) {
             beginShape();
             Point p = points.get(i);
-
-            stroke(p.col, 80, 80, 5);
+            stroke(p.col, 80, 80, 100);
             vertex(p.x, p.y);
             endShape();
         }
